a8378c6f8c934bb2ee946489e7c635604eb63f3c
Daniel Marc <49128637+danielmrcl@users.noreply.github.com>
2020-10-03 03:29:45 -0300

    operacoes basica de conta com saque, deposito, e consulta. futuramente adicionar outras

---
+++ Conta.java
@@ -0,0 +1,17 @@
+// package contabancaria;
+
+public class Conta {
+    private double saldo_conta = 0.0;
+
+    public double consultarSaldo() {
+        return this.saldo_conta;
+    }
+
+    public void depositarDinheiro(double valor) {
+        this.saldo_conta += valor;
+    }
+
+    public void debitarDinheiro(double valor) {
+        this.saldo_conta -= valor;
+    }
+}
