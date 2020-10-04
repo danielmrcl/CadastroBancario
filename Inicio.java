738383879a6d65079bb91c3aae549c83e613c0d2
Daniel Marc <49128637+danielmrcl@users.noreply.github.com>
2020-10-04 16:57:33 -0300

    menuPrincipal separado de Registrar

--- Inicio.java
+++ Inicio.java
@@ -10,84 +10,107 @@ public class Inicio {
 
         int menuEntrada = 0;
 
-        menuEntrada = Integer.parseInt(
-            JOptionPane.showInputDialog(
-            "OLÁ, BEM-VINDO(A) AO NOSSO SISTEMA BANCÁRIO!!\n" +
-            "Escolha um das opções abaixo: \n" +
-            "1 - Login \n" + 
-            "2 - Registrar \n" +
-            "0 - Sair"
-            )
-        );
-        switch(menuEntrada){
-            case 1: // Falta começar
-            case 2:
-                String log;
-                String senh;
-                String Agn;
-                JOptionPane.showMessageDialog(null, "Já que escolhei ser cadastrado, vamos lá.");
-                log = JOptionPane.showInputDialog(null, "Login: ");
-                senh = JOptionPane.showInputDialog(null, "Senha: ");
-                Agn = JOptionPane.showInputDialog(null, "Agência: ");
-                login1.Cadastro(log, senh, Agn);
-
-                // Escolhendo uma operação do menu
-                int menuPrincipal = 0;
-
-                do {
-                // Desenhando o menu principal
-                menuPrincipal = Integer.parseInt(
-                    JOptionPane.showInputDialog(
-                    "Escolha uma das Opções:\n" +
-                    "1 - Consultar Saldo;\n" +
-                    "2 - Depositar\n" +
-                    "3 - Debitar\n" +
-                    "0 - Sair"
+        do {
+            menuEntrada = Integer.parseInt(
+                JOptionPane.showInputDialog(
+                "OLÁ, BEM-VINDO(A) AO NOSSO SISTEMA BANCÁRIO!!\n" +
+                "Escolha um das opções abaixo: \n" +
+                "1 - Login \n" +
+                "2 - Registrar \n" +
+                "3 - Gerenciar conta\n" +
+                "0 - Sair"
                 )
-                );
+            );
 
-                switch (menuPrincipal) {
-                    case 0: // Operação 0 - Sair
+            switch(menuEntrada){
+                case 1: // Login
+                    String log1;
+                    String senh1;
 
-                        JOptionPane.showMessageDialog(null, "Saindo...");
+                    JOptionPane.showMessageDialog(null, "Já que escolheu logar, vamos lá.");
 
-                    break;
-                    case 1: // Operação 1 - Consultar saldo
+                    log1 = JOptionPane.showInputDialog(null, "Login: ");
+                    senh1 = JOptionPane.showInputDialog(null, "Senha: ");
 
-                        JOptionPane.showMessageDialog(null, "Seu saldo atual é de R$" + conta1.consultarSaldo());
-
-                    break;
-                    case 2: // Operação 2 - Depositar
-
-                        double depositoValor = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor a ser depositado na conta:"));
-
-                        conta1.depositarDinheiro(depositoValor);
+                    login1.Logar(log1, senh1);
 
                     break;
-                    case 3: // Operação 3 - debitarDinheiro
+                case 2: // Registrar
+                    String log2;
+                    String senh2;
+                    String agn2;
 
-                        double debitarValor = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor a ser debitado da conta:"));
+                    JOptionPane.showMessageDialog(null, "Já que escolheu ser cadastrado, vamos lá.");
 
-                        if (debitarValor > conta1.consultarSaldo()) {
-                            JOptionPane.showMessageDialog(null, "Este valor não pode ser debitado, tente outro.");
+                    log2 = JOptionPane.showInputDialog(null, "Login: ");
+                    senh2 = JOptionPane.showInputDialog(null, "Senha: ");
+                    agn2 = JOptionPane.showInputDialog(null, "Agência: ");
+
+                    login1.Cadastro(log2, senh2, agn2);
+
+                    break;
+                case 3: // Gerenciar conta
+
+                    // TODO: para gerenciar a conta precisa estar logado.
+
+                    // Escolhendo uma operação do menu
+                    int menuPrincipal = 0;
+
+                    do {
+                    // Desenhando o menu principal
+                    menuPrincipal = Integer.parseInt(
+                        JOptionPane.showInputDialog(
+                        "Escolha uma das Opções:\n" +
+                        "1 - Consultar Saldo;\n" +
+                        "2 - Depositar\n" +
+                        "3 - Debitar\n" +
+                        "0 - Voltar"
+                        )
+                    );
+
+                    switch (menuPrincipal) {
+                        case 0: // Operação 0 - Voltar
+
+                            JOptionPane.showMessageDialog(null, "Voltando...");
+
+                            break;
+                        case 1: // Operação 1 - Consultar saldo
+
+                            JOptionPane.showMessageDialog(null, "Seu saldo atual é de R$" + conta1.consultarSaldo());
+
+                            break;
+                        case 2: // Operação 2 - Depositar
+
+                            double depositoValor = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor a ser depositado na conta:"));
+
+                            conta1.depositarDinheiro(depositoValor);
+
+                            break;
+                        case 3: // Operação 3 - debitarDinheiro
+
+                            double debitarValor = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor a ser debitado da conta:"));
+
+                            if (debitarValor > conta1.consultarSaldo()) {
+                                JOptionPane.showMessageDialog(null, "Este valor não pode ser debitado, tente outro.");
+                            }
+                            else {
+                                conta1.debitarDinheiro(debitarValor);
+                            }
+
+                            break;
+                        default: // Operação não encontrada
+
+                            JOptionPane.showMessageDialog(null, "Operação Inválida, tente outra.");
+
+                            break;
                         }
-                        else {
-                            conta1.debitarDinheiro(debitarValor);
-                        }
-
-                        break;
-                    default: // Operação não encontrada
-
-                        JOptionPane.showMessageDialog(null, "Operação Inválida, tente outra.");
+                    } while (menuPrincipal != 0);
 
                     break;
-                    }
-                } while (menuPrincipal != 0);
-            case 0: 
-                JOptionPane.showMessageDialog(null, "Saindo...");
-                break;
-        }
-        
-        
+                case 0: // Sair
+                    JOptionPane.showMessageDialog(null, "Saindo...");
+                    break;
+            }
+        } while (menuEntrada != 0);
     }
 }
